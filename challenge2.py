153781e5d8115b0689837e43713bec319ba64b17
naufend <naufalmuhendraputra@gmail.com>
2026-01-22 17:23:14 +0700

    Add files via upload

---
+++ challenge2.py
@@ -0,0 +1,7 @@
+film = ['James Bond', 'Spongebob', 'Naruto', 'Monster Inc.', 'Samurai X']
+fav1 = film[0]
+fav2 = film[-4]
+
+print("Film Favorit saya adalah", fav1)
+print("Film Favorit kedua saya adalah", fav2)
+
