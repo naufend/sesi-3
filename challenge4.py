1d284462168f848b1af62f5f402b5fe0ef5c02a3
naufend <naufalmuhendraputra@gmail.com>
2026-01-22 18:09:11 +0700

    Add files via upload

--- challenge4.py
+++ challenge4.py
@@ -2,5 +2,5 @@ buah = ['durian', 'apel', 'mangga']
 sayur = ['selada', 'bawang', 'seledri']
 
 belanjaan = buah + sayur
-total = len(belanjaan)
-print(f'Saya perlu membeli {total} barang dari toko')
+
+print(f'Saya perlu membeli {len(belanjaan)} barang dari toko')
